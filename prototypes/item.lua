70f0ef34a141714fdb07557763f7fb14ad3cff6a
Maya <17090652+XuaTheGrate@users.noreply.github.com>
2022-04-21 21:40:29 +1200

    fix issue with steel void chest icons error

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -12,11 +12,11 @@ void_pipe.icon = nil
 void_pipe.icons = {{icon = data.raw.item["pipe"].icon, tint = voidTint}}
 void_pipe.order = void_pipe.order .. "a"
 
-local void_chest = table.deepcopy(data.raw.item["steel-chest"])
+local void_chest = table.deepcopy(data.raw.item["wooden-chest"])
 void_chest.name = "void-chest"
 void_chest.place_result = "void-chest"
 void_chest.icon = nil
-void_chest.icons = {{icon = data.raw.item["steel-chest"].icon, tint = voidTint}}
+void_chest.icons = {{icon = data.raw.item["wooden-chest"].icon, tint = voidTint}}
 void_chest.order = "a[items]-c[void-chest]"
 
 data:extend({
